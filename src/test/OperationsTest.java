e6212ac7ae0fb9bd9c065adef2babacbeb523ca7
ChristianMacias0 <cjavmaci@espol.edu.ec>
2025-01-15 16:01:16 -0500

    create OperationsTest.java

---
+++ src/test/OperationsTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class OperationsTest {
+    public static void main(String[] args) {
+        //code
+        System.out.println(calculatorsmp.Operations.Solve("1-1"));
+    }
+}
