1c8439b88b74dd85f298ebf29469edd92f5f97e5
Pamatalo05 <pmtapia@espol.edu.ec>
2025-01-15 16:50:07 -0500

    Update OperationsTest.java

--- test/calculatorsmp/OperationsTest.java
+++ test/calculatorsmp/OperationsTest.java
@@ -65,13 +65,36 @@ public class OperationsTest {
 
     @Test
     public void testSolveUsandoStringNumericoyAlfabetico() {
-        System.out.println("Solve");
+        System.out.println("Probar el funcionamiento del metodoSolve al recibir un String numerico y uno alfabetico ");
         String formula = "15+a";
         String expResult = "NumberFormatException";
         String result = Operations.Solve(formula);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
+    @Test
+    public void testSolveUsandoDosOperadoresSeguidosconStringsNumericos() {
+        System.out.println("Probar funcionamiento del método Solve si recibe 2 operadores seguidos en la formula ");
+        String formula = "2**2";
+        String expResult = " DobleOperadorException";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+    }
+
+
+    @Test
+    public void testSolveUsandoNumerosDecimales() {
+        System.out.println("Probar funcionamiento del método Solve si recibe números decimales");
+        String formula = "2.2+2";
+        String expResult = " 4.2";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        
+    }
+
+
+
+
+
+
 }
